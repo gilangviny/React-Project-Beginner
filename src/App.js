2975222ca7a2f0f45eabd52b6bb654888fdf008f
gilangviny <41935992+gilangviny@users.noreply.github.com>
2020-10-31 10:44:59 +0700

    make reminder birth using useState
    use useState and props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable react/button-has-type */
+/* eslint-disable no-console */
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button
+          onClick={() => {
+            setPeople([]);
+          }}
+        >
+          Clear All
+        </button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
